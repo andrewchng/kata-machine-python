defaf9c3839fcb93937935f45ec137ad9d686b29
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-06-17 17:10:24 +0800

    generate kata feature

---
+++ src/generate_kata.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+signature_map = {
+    "linear_search" : "def linear_search(haystack:list, needle:int) -> bool:",
+    "binary_search" : "def binary_search(haystack:list, needle:int) -> bool:"
+}
+
+def generate_new_kata():
+    folder_name = "day1"
+    src_dir = Path(folder_name)
+    src_dir.mkdir(exist_ok=True)
+
+    for kata_name, _ in signature_map.items():
+        kata_file = src_dir / f"{kata_name}.py"
+        with kata_file.open("w") as f:
+            f.write(f"{signature_map[kata_name]}")
+
+
+if __name__ == '__main__':
+    generate_new_kata()
